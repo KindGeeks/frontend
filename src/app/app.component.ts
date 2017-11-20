925b6e0fc60f6a03e7e866f7101ed2c7296e167f
Robert Young <apple@Robs-MacBook-Pro.local>
2017-11-20 21:03:39 +0000

    feat(css): Added papercss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Kind Geeks';
+  projects: Array<{ name: string, subtitle: string, description: string }> = [
+    {
+      name: 'Project A',
+      subtitle: 'What is the scope of project A',
+      description: `Notice that the card width in this example have been set to 20rem,
+      otherwise it will try to fill the current container/row where the card is.`
+    },
+    {
+      name: 'Project B',
+      subtitle: 'What is the scope of project B',
+      description: `Notice that the card width in this example have been set to 20rem,
+      otherwise it will try to fill the current container/row where the card is.`
+    },
+    {
+      name: 'Project C',
+      subtitle: 'What is the scope of project C',
+      description: `Notice that the card width in this example have been set to 20rem,
+      otherwise it will try to fill the current container/row where the card is.`
+    },
+  ];
 }
